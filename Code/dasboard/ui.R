4dd7fbeab1d2c66e412a32310c7db87e7983e2c5
alextabascio <alex.tabascio@mail.utoronto.ca>
2024-04-30 17:39:33 -0400

    ui changes

--- Code/dasboard/ui.R
+++ Code/dasboard/ui.R
@@ -30,24 +30,47 @@ dashboardPage(
                 column(width = 3,
                        # Description Panel
                        box(
-                         title = "How to Read: Visitor Level Measures",
-                         HTML("<p><b>Visits by Day of Week:</b></p>
-                              <p>This chart measures the quarterly total count of visits to buildings within the defined study area for the target and previous year based on the day of the week.</p>
-                              <p><b>Visits by Time of Day:</b></p>
-                              <p>This chart measures the quarterly total count of visits to buildings within the defined study area for the target and previous year based on the time of the day.</p>
-                              <p><b>Visitor Levels (%) Relative to 2019</b></p>
-                              <p>This chart measures the number of visits to buildings within the defined study area month by month, comparing the number of visits in 2019 to the same month of
-                              the current year to produce a relative percentage, with 100%, meaning the target month reached pre-pandemic levels.</p>
-                              <p><b>Visitor Levels Summary</b></p>
-                              <p>This table provides the percentage change of Visitor Levels, as defined in the previous chart, for the quarter to four distinct temporal baselines.
-                              <ul>
-                                <li>same quarter of the previous year</li>
-                                <li>same quarter of the pre-construction year (2022)</li>
-                                <li>same quarter of the pre-pandemic year</li>
-                                <li>the last quarter of the same year</li>
-                              </ul>"),
+                         title = "Chart Description",
+                         HTML("<p><b>Monthly Visitor Levels (%) Relative to 2019</b></p>
+                              <p>This chart measures the number of visits to buildings within the defined study area month by month, comparing the
+                              number of visits in 2019 to the same month of the current year to produce a relative percentage, with 100%, meaning
+                              the target month reached pre-pandemic levels.</p>"),
                          width = 12,
                          style = "margin 0px; padding 0px;"
+                       )
+                )
+              ),
+              fluidRow(
+                style = "margin: 0px; padding 0px;",
+                column(width = 9,
+                       # Visitor Type Plot
+                       box(
+                         title = "Quarterly Visit Count by Type of Visitor",
+                         plotOutput("VisitorTypePlot", height = "50vh"),
+                         width = 12,
+                         style = "margin: 0px; padding: 0px;"
+                       )
+                  
+                ),
+                column(width = 3,
+                       # Description Panel
+                       box(
+                         title = "Chart Description",
+                         HTML("<p><b>Quarterly Visit Count by Type of Visitor</b></p>
+                              <p>This chart measures the quarterly total of visits to buildings within the defined study area for the target
+                              and previous year based on the type of visitor.</p>
+                              <ul>
+                                <li>Visitors are placed into three groups using the common daytime and evening given by the Environics Analytics Mobile G.P.S. Data.</li>
+                                <ul>
+                                  <li>Common Daytime Location refers to the most common location of a device between 9 am and 5 pm. Used to infer work location.</li>
+                                  <li>Common Evening Location refers to the most common location of a device between 5 pm and 9 am. Used to infer home location.</li>
+                                </ul>
+                                <li>Resident – If a visitor's Common Evening Location <b>is within</b> 1 kilometre of the study area.</li>
+                                <li>Recurring Visitor – If a visitor's Common Daytime Location <b>is within</b> 1 kilometre of the study area <b>and</b> Common Evening
+                                Location <b>is not within</b> 1 kilometre of the study area.</li>
+                                <li>Infrequent Visitor - If a visitor's Common Daytime Location <b>is not within</b> 1 kilometre of the study area and
+                                Common Evening Location <b>is not within</b> 1 kilometre of the study area</li>"
+                              )
                        )
                 )
               ),
